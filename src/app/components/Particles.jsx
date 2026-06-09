f8053b161a44d97d5f2123e4fe42782bf105919e
Lidia Michalska <144784986+lmichalska@users.noreply.github.com>
2026-06-09 20:29:35 +0200

    particles, loader

---
+++ src/app/components/Particles.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export function Particles() {
+  return (
+    <div id="particle-container">
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+	<div className="particle"></div>
+</div>)}
